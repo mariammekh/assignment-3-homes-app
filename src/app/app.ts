4617ae89cad73081aac21cd6a5929098821612ff
mariammekh <mariammekhimar24@gmail.com>
2026-03-13 23:57:24 -0500

    Added routing to the angular application

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
-
+import {RouterModule} from '@angular/router'; 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Home],
